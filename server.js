6dcffc8f11082fe0e3f5713e03fc42d6a54ce7bf
azmiazharm <azmiazharm@gmail.com>
2022-06-04 17:20:47 +0700

    add env

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express"); //importing express
 const mongoose = require("mongoose")
 const app = express(); //making express object
 
-const port = 5000;
+const port = process.env.PORT || '5000';
 mongoose.connect( "mongodb+srv://azmi:kakatua2@capstone.zfr2c.mongodb.net/capstone?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
